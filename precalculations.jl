6c2224e95ac9881d5530961cdacaf6ba74127963
David Pascall <david.pascall@mac.com>
2024-07-10 11:13:29 +0100

    Change parallelisation to MPI/Elemental

--- precalculations.jl
+++ precalculations.jl
@@ -1,5 +1,6 @@
 #!/usr/bin/env julia
-using Random, Distributions, StringDistances, DelimitedFiles, LinearAlgebra
+##takes length of nucleotide string as an argument
+using Random, Distributions, StringDistances, DelimitedFiles, LinearAlgebra, MPI, MPIClusterManagers, Distributed
 
 function generate_seqs(nucleotides::Int) ##generate initial sequence set
     if nucleotides < 1
@@ -14,22 +15,33 @@ function hamming_similarity_mat(nucleotides::Int, seqs::Vector{String})
 end
 
 function calculate_covarience_matrix(nucleotides::Int, hamming_mat::Matrix{Int64}, rho::Float64, index::Float64)
-    return kron((hamming_mat./nucleotides).^index, ([[1, rho] [rho, 1]])) ##kronecker of seq correlation matrix and cross landscape correlation matrix
+    return kron(([[1, rho] [rho, 1]]), (hamming_mat./nucleotides).^index) ##kronecker of seq correlation matrix and cross landscape correlation matrix
 end
 
-seqs = generate_seqs(8)
-sim_mat = hamming_similarity_mat(8, seqs)
+man = MPIManager(np = 7)
+addprocs(man)
 
-open(joinpath(dirname(@__FILE__), "8HammingDistance.csv"), "w") do io
+nucleotides = parse(Int, ARGS[1])
+
+seqs = generate_seqs(nucleotides)
+sim_mat = hamming_similarity_mat(nucleotides, seqs)
+
+name = string(nucleotides) * "HammingDistance.csv"
+
+open(joinpath(dirname(@__FILE__), name), "w") do io
     writedlm(io, sim_mat, ',') ##write out hamming mat
 end
 
-Threads.@threads for rho in [-1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1] ##perform SVDs for each of the conditions
+covariance = Elemental.Matrix(Float64)
+
+for rho in [-1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1] ##perform SVDs for each of the conditions
     for i in [1.0, 5.0, 10.0]
-        decomp = svd(calculate_covarience_matrix(8, sim_mat, rho, i), full = true, alg = LinearAlgebra.QRIteration())
+        covariance = calculate_covarience_matrix(nucleotides, sim_mat, rho, i)
+        @mpi_do man decomp = svd(covariance, full = true, alg = LinearAlgebra.QRIteration())
+        factor = decomp.U * diagm(sqrt.(decomp.S))
         targetname = "factor_" * string(rho) * "_" * string(index) * ".csv"
         open(joinpath(dirname(@__FILE__), targetname), "w") do io
-            writedlm(io, decomp, ',') ##write out 
+            writedlm(io, factor, ',') ##write out 
         end
     end
 end
