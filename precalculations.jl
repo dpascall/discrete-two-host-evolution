e21f3a72b3a94d184f28981ac2642999674ea60f
David Pascall <david.pascall@mac.com>
2024-10-17 12:26:49 +0100

    Efficient matrix factor calculation (file actually commited)

--- precalculations.jl
+++ precalculations.jl
@@ -1,6 +1,6 @@
 #!/usr/bin/env julia
 ##takes length of nucleotide string as an argument
-using Random, Distributions, StringDistances, DelimitedFiles, LinearAlgebra, MPI, MPIClusterManagers, Distributed
+using Random, Distributions, StringDistances, DelimitedFiles, LinearAlgebra
 
 function generate_seqs(nucleotides::Int) ##generate initial sequence set
     if nucleotides < 1
@@ -14,12 +14,12 @@ function hamming_similarity_mat(nucleotides::Int, seqs::Vector{String})
     return nucleotides .- pairwise(Hamming(), seqs) ##generate pairwise hamming distances between all inputed sequences
 end
 
-function calculate_covarience_matrix(nucleotides::Int, hamming_mat::Matrix{Int64}, rho::Float64, index::Float64)
-    return kron(([[1, rho] [rho, 1]]), (hamming_mat./nucleotides).^index) ##kronecker of seq correlation matrix and cross landscape correlation matrix
+function calculate_covariance_factor(decomp_mu::Eigen, decomp_rho::Eigen, decomp_hamming::Eigen)
+    decomp_vectors = kron(decomp_mu.vectors, kron(decomp_rho.vectors, decomp_hamming.vectors)) ##generate final covariance matrix eigenvectors
+    decomp_values = kron(diagm(decomp_mu.values), kron(diagm(decomp_rho.values), diagm(decomp_hamming.values))) ##generate final covariance matrix eigenvalues
+    decomp_values[decomp_values .< 0] .= 0 ##fix negative eigenvalues to 0 - assumes true matrix PSD
+    return decomp_vectors * sqrt.(decomp_values) ##return factor
 end
-
-man = MPIManager(np = 7)
-addprocs(man)
 
 nucleotides = parse(Int, ARGS[1])
 
@@ -32,16 +32,17 @@ open(joinpath(dirname(@__FILE__), name), "w") do io
     writedlm(io, sim_mat, ',') ##write out hamming mat
 end
 
-covariance = Elemental.Matrix(Float64)
-
-for rho in [-1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1] ##perform SVDs for each of the conditions
-    for i in [1.0, 5.0, 10.0]
-        covariance = calculate_covarience_matrix(nucleotides, sim_mat, rho, i)
-        @mpi_do man decomp = svd(covariance, full = true, alg = LinearAlgebra.QRIteration())
-        factor = decomp.U * diagm(sqrt.(decomp.S))
-        targetname = "factor_" * string(rho) * "_" * string(index) * ".csv"
-        open(joinpath(dirname(@__FILE__), targetname), "w") do io
-            writedlm(io, factor, ',') ##write out 
+for index in [1.0, 5.0, Inf] ##perform SVDs for each of the conditions
+    decomp_hamming = eigen((sim_mat./nucleotides).^index) ##use decomposability to calculate each separately
+    for rho in [-1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]
+        decomp_rho = eigen([[1,rho] [rho,1]]) ##use decomposability to calculate each separately
+        for mu in [-1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]
+            decomp_mu = eigen([[1,mu] [mu,1]]) ##use decomposability to calculate each separately
+            factor = calculate_covariance_factor(decomp_mu, decomp_rho, decomp_hamming)
+            targetname = "factor_" * string(nucleotides) * "_" * string(mu) * "_" * string(rho) * "_" * string(index) * ".csv"
+            open(joinpath(dirname(@__FILE__), targetname), "w") do io
+                writedlm(io, factor, ',') ##write out 
+            end
         end
     end
-end
+#end
